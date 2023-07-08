4b8d3c86492166b739999cfab4f43117ae63c7f0
Cümşüd İsmayılov <c.smayilov1@gmail.com>
2023-07-08 18:03:13 +0400

    last code

--- src/js/app.js
+++ src/js/app.js
@@ -8,6 +8,18 @@ if (Item) {
      })
 }
 
+const Item1 = document.querySelector(".unitedclick");
+const List1 = document.querySelector(".united-hover");
+if (Item1) {
+    Item1.addEventListener("click",()=>{
+     console.log("salam");
+     List1.classList.toggle("united-active")
+ 
+     })
+}
+
+
+
 
 
 const Input = document.querySelector(".emails");
@@ -17,3 +29,41 @@ if (Input) {
         Input.classList.toggle("inputactive")
     })
 }
+
+
+
+
+
+
+const Navbar = document.querySelector(".desktop-nav");
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 300) {
+    if (Navbar) {
+      Navbar.classList.add("actice-nav");
+    }
+  } else {
+    if (Navbar) {
+      Navbar.classList.remove("actice-nav");
+    }
+  }
+});
+
+
+
+const ToTop = document.querySelector(".to-top");
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 500) {
+    if (ToTop) {
+      ToTop.style.display = "flex";
+    }
+  } else {
+    document.querySelector("body").style.background = "white";
+    if (ToTop) {
+      ToTop.style.display = "none";
+    }
+  }
+});
+ToTop &&
+  ToTop.addEventListener("click", () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  });
